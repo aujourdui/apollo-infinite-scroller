697ec699ae44e5c9740e2e733ae45dcf8baf1512
ito <kyosuke.ito@gmail.com>
2022-06-18 13:58:03 -0700

    create basic pagination

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,31 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
-  type Books {
-    info: Info
-    results: [Book]
-  }
+  # type Books {
+  #   info: Info
+  #   results: [Book]
+  # }
 
-  type Book {
+  # type Book {
+  #   title: String
+  #   author: String
+  # }
+  type Books {
     title: String
     author: String
   }
 
-  type Info {
-    count: Int
-    pages: Int
-    next: Int
-    prev: Int
-  }
+  # type Info {
+  #   count: Int
+  #   pages: Int
+  #   next: Int
+  #   prev: Int
+  # }
 
   type Query {
-    books(page: Int): [Books!]
+    books(limit: Int): [Books!]
+    # books(): [Books!]
   }
-  # type Query {
-  #   books: [Book(page: Int!)]: Int
-  # }
 `;
 
 const bookInfo = [
@@ -83,17 +85,272 @@ const bookInfo = [
         title: "Book12",
         author: "Mike",
       },
+      {
+        title: "Book13",
+        author: "John",
+      },
+      {
+        title: "Book14",
+        author: "Mike",
+      },
+      {
+        title: "Book15",
+        author: "John",
+      },
+      {
+        title: "Book16",
+        author: "Mike",
+      },
+      {
+        title: "Book17",
+        author: "John",
+      },
+      {
+        title: "Book18",
+        author: "Mike",
+      },
+      {
+        title: "Book19",
+        author: "John",
+      },
+
+      {
+        title: "Book20",
+        author: "Mike",
+      },
+      // {
+      //   title: "Book21",
+      //   author: "John",
+      // },
+      // {
+      //   title: "Book22",
+      //   author: "Mike",
+      // },
+      // {
+      //   title: "Book23",
+      //   author: "John",
+      // },
+      // {
+      //   title: "Book24",
+      //   author: "Mike",
+      // },
+      // {
+      //   title: "Book25",
+      //   author: "John",
+      // },
+      // {
+      //   title: "Book26",
+      //   author: "Mike",
+      // },
+      // {
+      //   title: "Book27",
+      //   author: "John",
+      // },
+      // {
+      //   title: "Book28",
+      //   author: "Mike",
+      // },
+      // {
+      //   title: "Book29",
+      //   author: "John",
+      // },
+      // {
+      //   title: "Book30",
+      //   author: "Mike",
+      // },
+      // {
+      //   title: "Book31",
+      //   author: "John",
+      // },
+      // {
+      //   title: "Book32",
+      //   author: "Mike",
+      // },
+      // {
+      //   title: "Book33",
+      //   author: "John",
+      // },
+      // {
+      //   title: "Book34",
+      //   author: "Mike",
+      // },
+      // {
+      //   title: "Book35",
+      //   author: "John",
+      // },
+      // {
+      //   title: "Book36",
+      //   author: "Mike",
+      // },
     ],
+  },
+];
+const booksBasic = [
+  {
+    title: "Book1",
+    author: "John",
+  },
+  {
+    title: "Book2",
+    author: "Mike",
+  },
+  {
+    title: "Book3",
+    author: "John",
+  },
+  {
+    title: "Book4",
+    author: "Mike",
+  },
+  {
+    title: "Book5",
+    author: "John",
+  },
+  {
+    title: "Book6",
+    author: "Mike",
+  },
+  {
+    title: "Book7",
+    author: "John",
+  },
+  {
+    title: "Book8",
+    author: "Mike",
+  },
+  {
+    title: "Book9",
+    author: "John",
+  },
+  {
+    title: "Book10",
+    author: "Mike",
+  },
+  {
+    title: "Book11",
+    author: "John",
+  },
+  {
+    title: "Book12",
+    author: "Mike",
+  },
+  {
+    title: "Book13",
+    author: "John",
+  },
+  {
+    title: "Book14",
+    author: "Mike",
+  },
+  {
+    title: "Book15",
+    author: "John",
+  },
+  {
+    title: "Book16",
+    author: "Mike",
+  },
+  {
+    title: "Book17",
+    author: "John",
+  },
+  {
+    title: "Book18",
+    author: "Mike",
+  },
+  {
+    title: "Book19",
+    author: "John",
+  },
+
+  {
+    title: "Book20",
+    author: "Mike",
+  },
+  {
+    title: "Book21",
+    author: "John",
+  },
+  {
+    title: "Book22",
+    author: "Mike",
+  },
+  {
+    title: "Book23",
+    author: "John",
+  },
+  {
+    title: "Book24",
+    author: "Mike",
+  },
+  {
+    title: "Book25",
+    author: "John",
+  },
+  {
+    title: "Book26",
+    author: "Mike",
+  },
+  {
+    title: "Book27",
+    author: "John",
+  },
+  {
+    title: "Book28",
+    author: "Mike",
+  },
+  {
+    title: "Book29",
+    author: "John",
+  },
+  {
+    title: "Book30",
+    author: "Mike",
+  },
+  {
+    title: "Book31",
+    author: "John",
+  },
+  {
+    title: "Book32",
+    author: "Mike",
+  },
+  {
+    title: "Book33",
+    author: "John",
+  },
+  {
+    title: "Book34",
+    author: "Mike",
+  },
+  {
+    title: "Book35",
+    author: "John",
+  },
+  {
+    title: "Book36",
+    author: "Mike",
   },
 ];
 
 const resolvers = {
   Query: {
-    books: (parent, arg, context, info) =>
-      //  console.log(parent),
-      // console.log(arg),
-
-      bookInfo,
+    // books: async (parent, arg, context, info) => books,
+    books: async (_parent, args, _context, _info) => {
+      if (args.limit) {
+        // const end = args.limit + args.offset;
+        // const start = args.offset;
+        const end = args.limit;
+        // const start = args.offset;
+        // console.log(args.limit);
+        return [...booksBasic].slice(0, end);
+        // return [...booksBasic];
+      } else {
+        return [...booksBasic];
+        // return [...books].slice(start, end);
+      }
+    },
+    // books: (parent, arg, context, info) => bookInfo,
   },
 };
 
--- src/graphql/queries/books.js
+++ src/graphql/queries/books.js
@@ -1,17 +1,25 @@
 import { gql } from "@apollo/client";
 
+// export const GET_BOOKS = gql`
+//   query getBooks($page: Int, $limit: Int) {
+//     books(page: $page, limit: $limit) {
+//       info {
+//         count
+//         pages
+//         next
+//       }
+//       results {
+//         title
+//         author
+//       }
+//     }å
+//   }
+// `;
 export const GET_BOOKS = gql`
-  query getBooks($page: Int) {
-    books(page: $page) {
-      info {
-        count
-        pages
-        next
-      }
-      results {
-        title
-        author
-      }
+  query getBooks($limit: Int) {
+    books(limit: $limit) {
+      title
+      author
     }
   }
 `;
